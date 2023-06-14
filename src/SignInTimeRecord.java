8531ec2de929136147137a181db85162877c02f5
alvanting <alvanting@hotmail.com>
2023-06-14 15:28:34 +0800

    Mini Test

---
+++ src/SignInTimeRecord.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+public class SignInTimeRecord {
+    private Date signIn;
+
+    public SignInTimeRecord(Date signIn) {
+        this.signIn = signIn;
+    }
+}
---
+++ src/SignOutTimeRecord.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class SignOutTimeRecord {
+    private Date signOut;
+
+    public SignOutTimeRecord(Date signOut) {
+        this.signOut = signOut;
+    }
+}
